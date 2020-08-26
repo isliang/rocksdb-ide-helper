8511913aeb5561fa47a07a6bc230460346944f6d
isliang <yesuhuangsi@163.com>
2020-08-26 15:52:05 +0800

    add ide helper

---
+++ README.md
@@ -0,0 +1,3 @@
+## IDE Helper for RocksDB PHP Extension
+
+This is an ide helper for rocksdb php extension for [rocksdb-php](https://github.com/isliang/rocksdb-php)
---
+++ src/RocksDB.php
@@ -0,0 +1,70 @@
+<?php
+namespace RocksDB;
+
+class RocksDB
+{
+    /**
+     * RocksDB constructor.
+     * @param $name string path to rocksdb
+     */
+    public function __construct($name)
+    {
+
+    }
+
+    /**
+     * @param bool $readonly
+     * @return bool
+     * Open the database with the specified "name"
+     * if readonly is true, Open the database for read only
+     */
+    public function open($readonly = false)
+    {
+        return true;
+    }
+
+    /**
+     * @param $key string
+     * @param $value string
+     * @param string $column
+     * @return bool
+     * Set the database entry for "key" to "value".
+     * If "key" already exists, it will be overwritten.
+     */
+    public function put($key, $value, $column = 'default')
+    {
+        return true;
+    }
+
+    /**
+     * @param $key string
+     * @param string $column
+     * @return string
+     * If the database contains an entry for "key" return the
+     * corresponding value.
+     */
+    public function get($key, $column = 'default')
+    {
+        return "";
+    }
+
+    /**
+     * @param $keys string[]
+     * @param $columns string[]
+     * @return string[]
+     * Consistent Get of many keys across column families
+     */
+    public function mGet($keys, $columns = [])
+    {
+        return [];
+    }
+
+    /**
+     * @return string
+     * return the last error
+     */
+    public function lastError()
+    {
+        return "";
+    }
+}
